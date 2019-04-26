18fe8ecfc5a93071abf2df7690bd050b0f6203fe
catherine <catherine_pjm@163.com>
2019-04-26 08:41:32 +0800

    a simple project of websocket

---
+++ demo1/wsServer.js
@@ -0,0 +1,17 @@
+var ws = require("nodejs-websocket")
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+    conn.on("error",function(err){
+    	console.log("handle err")
+    	console.log(err)
+    })
+  // handle error event
+}).listen(8001)
